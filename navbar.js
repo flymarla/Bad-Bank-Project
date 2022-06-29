fb179123ac12a2c916f06e6f462bf275aa7eefa7
Marla Hansen <marla_hansen@yahoo.com>
2022-06-29 08:39:17 -0400

    Added bank symbol, right justify

--- navbar.js
+++ navbar.js
@@ -4,11 +4,14 @@ const Navbar = (props) => {
         <>
         <nav className="navbar navbar-expand-lg" style={{backgroundColor:"#7FB5FF"}} >
         <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+                <img src="bank.png" alt="banking symbol" width="30" height="24"/>
+            </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li className="nav-item">
                 <a className="nav-link" id="home" href="#">Home <p className="tooltiptext">Your Bank Homepage</p></a>
                 </li>
